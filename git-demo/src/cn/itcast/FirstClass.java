0b211dd9314022bcb0de09fcdcffce9aa3b8daa9
itcast <bos@itcast.cn>
2019-06-12 19:05:33 +0800

    提交git-demo项目

---
+++ git-demo/src/cn/itcast/FirstClass.java
@@ -0,0 +1,18 @@
+package cn.itcast;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("i love you ");
+        System.out.println("i love you ");
+        System.out.println("i love you ");
+        System.out.println("i love you ");
+        System.out.println("i love you ");
+        System.out.println("i love you ");
+        System.out.println("i love you ");
+        System.out.println("i love you ");
+        System.out.println("i love you ");
+
+
+    }
+}
